dc8800f47f034e63bafe9b497c1f7ddd2922f4fa
Андрей Карабанов <deskpa@yandex.ru>
2023-10-17 23:47:19 +0300

    Created index.xhtml and main.xhtml. Added the clock

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+    id("war")
+}
+
+group = "org.forafox"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("jakarta.servlet:jakarta.servlet-api:6.0.0")
+    compileOnly("jakarta.servlet.jsp:jakarta.servlet.jsp-api:3.1.1")
+    implementation("org.postgresql:postgresql:42.6.0")
+    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+    compileOnly ("org.projectlombok:lombok:1.18.20")
+    annotationProcessor ("org.projectlombok:lombok:1.18.20")
+    compileOnly("jakarta.enterprise:jakarta.enterprise.cdi-api:4.0.1")
+    compileOnly("jakarta.platform:jakarta.jakartaee-web-api:10.0.0")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ src/main/webapp/resources/scripts/clock.js
@@ -0,0 +1,27 @@
+const dayOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+window.addEventListener("DOMContentLoaded", () => {
+    let clockFromDoc = document.getElementById("clock");
+    let dayFromDoc = document.getElementById("dayOfWeek");
+    let setTime=()=> {
+        let date = new Date();
+
+        let time = [date.getHours(), date.getMinutes(), date.getSeconds()]; // |[0] = Hours| |[1] = Minutes| |[2] = Seconds|
+
+        if (time[0] < 10) {
+            time[0] = "0" + time[0];
+        }
+        if (time[1] < 10) {
+            time[1] = "0" + time[1];
+        }
+        if (time[2] < 10) {
+            time[2] = "0" + time[2];
+        }
+
+        clockFromDoc.innerHTML = [time[0], time[1], time[2]].join(':');
+        dayFromDoc.innerHTML = dayOfWeek[date.getDay()] + " " + date.getDay() + " " + months[date.getMonth()] + " " + date.getFullYear();
+    }
+    setTime()
+    setInterval(setTime, 7000);
+})
